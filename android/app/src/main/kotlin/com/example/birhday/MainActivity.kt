4cc7dfedae921eb954d43ae024d7a73b095c02c5
Esraa Nagy <en1228@fayoum.edu.eg>
2024-10-08 16:48:12 +0300

    * adding birthday card photo

---
+++ android/app/src/main/kotlin/com/example/birhday/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.birhday
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const BirthdayCard());
+}
+
+class BirthdayCard extends StatelessWidget {
+  const BirthdayCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Color(0xffD2BCD5),
+        body: Image(
+          image: AssetImage("assets/images/birthday.png"),
+        ),
+      ),
+    );
+  }
+}
